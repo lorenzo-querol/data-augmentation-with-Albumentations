32e4c5ec2c299df3fbee78cd789dd29fc870b95f
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-03-19 18:05:29 +0800

    (add) Helper text to transform file

--- transforms.py
+++ transforms.py
@@ -1,5 +1,9 @@
 import albumentations as A
 
+"""
+Contains all the transforms that are applied to the images
+You can add more to this pipeline, visit https://albumentations.ai/docs/ for more
+"""
 transform = A.Compose([
     A.HorizontalFlip(),
     A.VerticalFlip(),
