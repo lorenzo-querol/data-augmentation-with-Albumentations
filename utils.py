416c9723634ac60feafda2c5163c5f38368016cc
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-03-19 17:33:45 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import cv2 as cv
+import glob
+
+def check_img_dir(dir):
+    """
+    Check if an image directory is valid
+    Input: 
+    - dir: directory to check
+
+    Output:
+    - True if directory is valid, False otherwise
+    """
+    if not os.path.isdir(dir):
+        print('%s is not a valid directory.' % dir)
+        sys.exit(1)
+
+    return True
+
+def check_dest_dir(dir):
+    """
+    Check if a destination directory exists, if not, create it
+    """
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+def read_images(img_dir):
+    """
+    Read all images in a directory
+
+    Input:
+    - img_dir: directory to read images from
+
+    Output:
+    - list of image file paths
+    """
+
+    return glob.glob(f'{img_dir}/*.JPG', recursive=True)
